d37c161d63c281f480fc56efe77da7ac8844bbe4
Daniel Minarik <daniel.minarik@gmail.com>
2017-01-27 10:36:18 +0100

    Theme + devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126213141) do
+ActiveRecord::Schema.define(version: 20170126214729) do
 
   create_table "issues", force: :cascade do |t|
     t.string   "name"
