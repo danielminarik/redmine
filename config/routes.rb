a4a488f4f67927fbce37d146f6981ca1a8a0073d
Daniel Minarik <daniel.minarik@gmail.com>
2017-01-31 15:01:41 +0100

    added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'issues#index'
+  authenticated :user do
+    root :to => "issues#index"
+  end
+  root 'welcome#index'
   resources :issues
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
