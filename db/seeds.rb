430fc902fee8ca56b4da475e171e9ce101399cd0
Daniel Minarik <daniel.minarik@gmail.com>
2017-02-03 10:15:04 +0100

    creating admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Tracker.create :name => "Bug"
-Tracker.create :name => "Feature"
-Tracker.create :name => "Devel"
-Tracker.create :name => "Support"
-Status.create :name => "New"
-Status.create :name => "In Progress"
-Status.create :name => "Closed"
-Priority.create :name => "Low"
-Priority.create :name => "Normal"
-Priority.create :name => "High"
+# Tracker.create :name => "Bug"
+# Tracker.create :name => "Feature"
+# Tracker.create :name => "Devel"
+# Tracker.create :name => "Support"
+# Status.create :name => "New"
+# Status.create :name => "In Progress"
+# Status.create :name => "Closed"
+# Priority.create :name => "Low"
+# Priority.create :name => "Normal"
+# Priority.create :name => "High"
+
+user = User.find_or_create_by(email: "admin@mail.com")
+user.password = "heslo1"
+user.admin = true
+user.save
