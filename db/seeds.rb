2d1ab7de4fe10f10850c97d809eafb92a62f987a
Daniel Minarik <daniel.minarik@gmail.com>
2017-02-03 09:23:23 +0100

    removed duplicit values and updatd seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tracker.create :name => "Bug"
+Tracker.create :name => "Feature"
+Tracker.create :name => "Devel"
+Tracker.create :name => "Support"
+Status.create :name => "New"
+Status.create :name => "In Progress"
+Status.create :name => "Closed"
+Priority.create :name => "Low"
+Priority.create :name => "Normal"
+Priority.create :name => "High"
